f953c951075d6c4e053cb8688b099e7d7da5355e
Cenk Kılıç <cenk@kilic.dev>
2020-06-13 19:36:39 +0200

    fix: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  "extends": ["@cenk1cenk2/eslint-config/typescript"],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: '@root/**',
+            group: 'parent'
+          }
+        ],
+        pathGroupsExcludedImportTypes: [
+          'builtin'
+        ],
+        groups: [
+          [
+            'builtin',
+            'external'
+          ],
+          [
+            'index',
+            'parent',
+            'sibling'
+          ]
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
+  }
+}
